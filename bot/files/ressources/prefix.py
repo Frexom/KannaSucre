f3153770bac9f334cdaf3c278b3eab7ac5bf6263
Frexom <77272835+Frexom@users.noreply.github.com>
2022-03-17 10:00:37 +0100

    Add files via upload

---
+++ bot/files/ressources/prefix.py
@@ -0,0 +1,12 @@
+import discord
+from connection import *
+
+
+async def get_pre(message, message2 = None):
+  if(message2 != None):
+    message = message2
+  connection, cursor = await get_conn("./files/ressources/bot.db")
+  await cursor.execute("SELECT guild_prefix FROM guilds WHERE guild_id = ?", (message.guild.id, ))
+  result = await cursor.fetchone()
+  await close_conn(connection, cursor)
+  return result[0]
