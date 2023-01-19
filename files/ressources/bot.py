30e944ae185796520f7fddef3250f959c72fdcd8
Frexom <hugom2003@sfr.fr>
2023-01-19 11:55:15 +0100

    Added 9G Some pokémons are still missing, like Peldean wooper, and that may cause errors Also solved a bug which was preventing the bot too use the translator, resulting in every command crashing

--- files/ressources/bot.py
+++ files/ressources/bot.py
@@ -1,5 +1,4 @@
 import datetime
-import discord
 import asyncio
 import random
 import time
@@ -7,31 +6,15 @@ import sys
 import os
 
 from discord import app_commands
-from discord.ext import commands
 from discord.ext import tasks
 from dotenv import load_dotenv
 from typing import Union
 
+from persistent import *
 from locales import *
 from prefix import *
-from ui import *
+
 
 load_dotenv()
-
-class PersistentBot(commands.Bot):
-    def __init__(self):
-        intents = discord.Intents.default()
-        intents.members = True
-        intents.messages = True
-        intents.message_content = True
-
-        super().__init__(command_prefix=get_pre, intents=intents)
-        self.remove_command('help')
-
-        self.translator = Translator()
-
-
-    async def setup_hook(self) -> None:
-        self.add_view(giveawayView())
 
 bot = PersistentBot()
