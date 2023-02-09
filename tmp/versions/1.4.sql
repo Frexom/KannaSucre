5668821560a78da486b760ff4d81bd28724551d1
Frexom <hugom2003@sfr.fr>
2023-02-09 19:37:21 +0100

    Added missing prefix commands (addlevel and editlevels) -Created turkish localization files (WIP) -Updated the databse and the 1_4.sql script

--- tmp/versions/1.4.sql
+++ tmp/versions/1.4.sql
@@ -13,4 +13,8 @@ CREATE TABLE gld_level (
     PRIMARY KEY("guild_id", "user_id")
 );
 
+INSERT INTO com_command(com_name, com_short, com_desc, com_use_example, com_user_perms, com_bot_perms, cat_category) VALUES ("addlevel", "Adds a level reward", "Set up and automations to give roles to your members when they reach a certain KannaSucre level!", "addlevel 5 @Role", "manage_guild", "manage_roles", 1);
+INSERT INTO com_command(com_name, com_short, com_desc, com_use_example, com_user_perms, com_bot_perms, cat_category) VALUES ("editlevels", "Edits the level rewards", "View and delete the current's server level rewards!", "editlevels", "manage_guild", "manage_roles", 1);
+
+
 INSERT INTO db_updates(updates) VALUES ("1.4 Done!");
